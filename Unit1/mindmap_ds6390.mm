5b449d7647820ec84701799a390f747ba1cec4f2
Ben Goodwin <70025930+bgoodwinSMU@users.noreply.github.com>
2021-04-27 19:45:06 -0600

    Add files via upload

---
+++ Unit1/mindmap_ds6390.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1619573823655" ID="ID_230486990" MODIFIED="1619573903991" TEXT="New Project">
+<node CREATED="1619573913994" ID="ID_1331741797" MODIFIED="1619574074109" POSITION="right" TEXT="Objectives">
+<node CREATED="1619573972792" ID="ID_326313685" MODIFIED="1619574035094" TEXT="Build team">
+<node CREATED="1619574100646" ID="ID_1316620356" MODIFIED="1619574116313" TEXT="Develop team with best attributes for project"/>
+</node>
+<node CREATED="1619574043316" ID="ID_441511711" MODIFIED="1619574070983" TEXT="Design Project">
+<node CREATED="1619574119132" ID="ID_720778338" MODIFIED="1619574136640" TEXT="Use all tools and available resources to foster innovation"/>
+</node>
+<node CREATED="1619574074110" ID="ID_1356779104" MODIFIED="1619574095508" TEXT="Maintain budget">
+<node CREATED="1619574139154" ID="ID_27487350" MODIFIED="1619574148519" TEXT="Keep a close eye on overruns"/>
+</node>
+</node>
+</node>
+</map>
